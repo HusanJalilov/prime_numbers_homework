ea5752e881769f1a67745e347f620f5db7a57805
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-23 17:40:08 +0500

    Task 01

--- is_prime.py
+++ is_prime.py
@@ -1,4 +1,3 @@
-#Define function, Check if number is prime.
 def is_prime(n:int)->bool:
     """
     Check if number is prime.
@@ -7,5 +6,10 @@ def is_prime(n:int)->bool:
     Returns:
         bool: True if prime, False if not.
     """
- 
-    return 
+    if n == 1:
+        return False
+    
+    for  i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
